16cc71f1e495646b1e8df5889e649373e0240fcd
Samer Khamisi <samer.n.khamisi@outlook.com>
2021-04-01 08:09:06 -0400

    added C# HW20

--- Exercises/CEXR10/Program.cs
+++ Exercises/CEXR10/Program.cs
@@ -6,56 +6,79 @@ namespace CEXR10
     {
         static void Main(string[] args)
         {
-            Console.Write("Please enter an integer to convert: ");
-            int input = int.Parse(Console.ReadLine());
-            Console.Write("Please choose a base to convert from [2] [8] [10] [16]: ");
-            int baseNum = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine($"[Number: {input}] [Base: {baseNum}]");
-            Console.WriteLine("----------------------");
+            while (true)
+            {
+                Console.Write("\nPlease enter an integer to convert: ");
+                string tryInput = Console.ReadLine();
+                int.TryParse(tryInput, out int input);
+                Console.Write("Please choose a base to convert from [2] [8] [10] [16]: \n");
+                int baseNum = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine($"[Number: {input}] [Base: {baseNum}]");
+                Console.WriteLine("------------------------");
 
-            int result;
-            string hexResult;
+                int result;
+                string hexResult;
 
-            if (baseNum == 2)
-            {
-                result = BinToDec(input);
-                Console.WriteLine($"Decimal conversion is {result}");
-                result = BinToOct(input);
-                Console.WriteLine($"Octal conversion is {result}");
-                result = BinToHex(input);
-                Console.WriteLine($"Hex conversion is {result}");
-            }
-            else if (baseNum == 8)
-            {
-                result = OctToBin(input);
-                Console.WriteLine($"Binary conversion is {result}");
-                result = OctToDec(input);
-                Console.WriteLine($"Decimal conversion is {result}");
-                hexResult = OctToHex(input);
-                Console.WriteLine($"Hex conversion is {hexResult}");
-            }
-            else if (baseNum == 10)
-            {
-                result = DecToBin(input);
-                Console.WriteLine($"Binary conversion is {result}");
-                result = DecToOct(input);
-                Console.WriteLine($"Octal conversion is {result}");
-                hexResult = DecToHex(input);
-                Console.WriteLine($"Hex conversion is {hexResult}");
-            }
-            else if (baseNum == 16)
-            {
-                result = HexToBin(input);
-                Console.WriteLine($"Binary conversion is {result}");
-                result = HexToOct(input);
-                Console.WriteLine($"Octal conversion is {result}");
-                result = HexToDec(input);
-                Console.WriteLine($"Decimal conversion is {result}");
-            }
-            else
-            {
-                Console.WriteLine("Error in base to convert from");
+                if (baseNum == 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    result = BinToDec(input);
+                    Console.WriteLine($"Decimal conversion is {result}");
+                    result = BinToOct(input);
+                    Console.WriteLine($"Octal conversion is {result}");
+                    hexResult = BinToHex(input);
+                    Console.WriteLine($"Hex conversion is {hexResult}");
+                }
+                else if (baseNum == 8)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    result = OctToBin(input);
+                    Console.WriteLine($"Binary conversion is {result}");
+                    result = OctToDec(input);
+                    Console.WriteLine($"Decimal conversion is {result}");
+                    hexResult = OctToHex(input);
+                    Console.WriteLine($"Hex conversion is {hexResult}\n");
+                }
+                else if (baseNum == 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    result = DecToBin(input);
+                    Console.WriteLine($"Binary conversion is {result}");
+                    result = DecToOct(input);
+                    Console.WriteLine($"Octal conversion is {result}");
+                    hexResult = DecToHex(input);
+                    Console.WriteLine($"Hex conversion is {hexResult}\n");
+                }
+                else if (baseNum == 16)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    result = HexToBin(tryInput);
+                    Console.WriteLine($"Binary conversion is {result}");
+                    result = HexToOct(tryInput);
+                    Console.WriteLine($"Octal conversion is {result}");
+                    result = HexToDec(tryInput);
+                    Console.WriteLine($"Decimal conversion is {result}\n");
+                }
+                else
+                {
+                    Console.WriteLine("Error in base to convert from");
+                }
+
+                Console.ResetColor();
+                Console.WriteLine("\nEnter [Q] to Exit || [N] to Continue");
+                string exit = Console.ReadLine();
+
+                if (exit == "Q" || exit == "q")
+                {
+                    Console.WriteLine("\nGoodbye!");
+                    Environment.Exit(0);
+                }
+                else if (exit == "N" || exit == "n");
+                {
+                    Console.Clear();
+                }
             }
         }
     }
@@ -65,31 +88,18 @@ namespace CEXR10
         static int Answer { get; set; }
         static int A;
         static int B;
-        static int C;
-        static int D;
-        static int E;
 
         /// From Binary Conversion ///
         public static int BinToOct(int input)
         {
-            int factor = 1;
-
-            while (input > 0)
-            {
-                A = input % 1000;
-                B = A % 10;
-                C = ((A / 10) % 10) * 2;
-                D = (A / 100) * 4;
-                E = B + C + D;
-                Answer += E * factor;
-                factor *= 10;
-                input = input / 1000;
-            }
+            int input2 = BinToDec(input);
+            Answer = DecToOct(input2);
             return Answer;
         }
 
         public static int BinToDec(int input)
         {
+            Answer = 0;
             double power = 0;
 
             while (input > 0)
@@ -103,109 +113,81 @@ namespace CEXR10
             return (int)Answer;
         }
 
-        public static int BinToHex(int input)
+        public static string BinToHex(int input)
         {
-            int answer = 0;
-
-            while (input > 0)
-            {
-     
-            }
-            return (int)answer;
+            int input2 = BinToDec(input);
+            string answer = DecToHex(input2);
+            return answer;
         }
 
         /// From Octal Conversion ///
         public static int OctToBin(int input)
         {
-            int factor = 1;
-            int Answer1 = 0;
-            int b1 = 0;
-            int a2 = 0;
+            int power = 1;
 
             while (input > 0)
             {
-                a2 = input % 10;
-                b1 = DecToBin((int)a2);
-                Answer1 += b1 * factor;
-                factor *= 1000;
+                A = input % 10;
+                B = DecToBin((int)A);
+                Answer += B * power;
+                power *= 1000;
                 input = input / 10;
             }
-            return (int)Answer1;
+            return Answer;
         }
 
         public static int OctToDec(int input)
         {
             int answer = 0;
-            int factor = 1;
+            int power = 1;
 
             while (input > 0)
             {
                 A = input % 10;
                 input = input / 10;
-                answer = (int)A * factor;
-                factor = factor * 8;
+                answer = (int)A * power;
+                power = power * 8;
             }
             return answer;
         }
 
         public static string OctToHex(int input)
         {
-            char[] hexNum = new char[100];
-            int i = 0;
-
             int input2 = OctToDec(input);
-
-            while (input2 > 0)
-            {
-                A = input2 % 16;
-                if (A < 10)
-                {
-                    hexNum[i] = (char)(A + 48);
-                    i++;
-                }
-                else if (A >= 10)
-                {
-                    hexNum[i] = (char)(A + 55);
-                    i++;
-                }
-                input2 = input2 / 16;
-            }
-            Array.Reverse(hexNum);
-            string strHex = new string(hexNum);
-            return strHex;
+            string answer = DecToHex(input2);
+            return answer;
         }
 
         /// From Decimal Conversion ///
         public static int DecToBin(int input)
         {
             int answer = 0;
-            int factor = 1;
+            int power = 1;
 
             while (input > 0)
             {
                 A = input % 2;
                 input = input / 2;
-                answer += (int)A * factor;
-                factor *= 10;
+                answer += (int)A * power;
+                power *= 10;
             }
             return answer;
         }
 
         public static int DecToOct(int input)
         {
-            int answer = 0;
-            int factor = 1;
+            Answer = 0;
+            int power = 1;
 
             while (input > 0)
             {
                 A = input % 8;
                 input = input / 8;
-                answer += (int)A * factor;
-                factor *= 10;
+                Answer += (int)A * power;
+                power *= 10;
             }
-            return answer;
+            return Answer;
         }
-
 
         public static string DecToHex(int input)
         {
@@ -220,7 +202,7 @@ namespace CEXR10
                     hexNum[i] = (char)(A + 48);
                     i++;
                 }
-                else if(A >= 10)
+                else if (A >= 10)
                 {
                     hexNum[i] = (char)(A + 55);
                     i++;
@@ -233,38 +215,41 @@ namespace CEXR10
         }
 
         /// From Hex Conversion ///
-
-        public static int HexToBin(int input)
+        public static int HexToBin(string input)
         {
-            int answer = 0;
-            while (input > 0)
-            {
-
-            }
-            return (int)answer;
+            int input2 = HexToDec(input);
+            Answer = DecToBin(input2);
+            return Answer;
         }
 
-
-        public static int HexToOct(int input)
+        public static int HexToOct(string input)
         {
-            int answer = 0;
-            while (input > 0)
-            {
-
-            }
-            return (int)answer;
+            int input2 = HexToDec(input);
+            Answer = DecToOct(input2);
+            return Answer;
         }
 
-
-        public static int HexToDec(int input)
+        public static int HexToDec(string input)
         {
-            int answer = 0;
+            int length = input.Length;
 
-            while (input > 0)
+            int power = 1;
+            Answer = 0;
+
+            for (int i = length - 1; i >= 0; i--)
             {
-
+                if (input[i] >= '0' && input[i] <= '9')
+                {
+                    Answer += (input[i] - 48) * power;
+                    power = power * 16;
+                }
+                else if (input[i] >= 'A' && input[i] <= 'F')
+                {
+                    Answer += (input[i] - 55) * power;
+                    power = power * 16;
+                }
             }
-            return (int)answer;
+            return Answer;
         }
     }
 }
