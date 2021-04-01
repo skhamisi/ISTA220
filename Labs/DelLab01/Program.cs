da62eaf4d5d98e27a48f27e6d9841315a9fe66d6
Samer Khamisi <samer.n.khamisi@outlook.com>
2021-04-01 08:13:21 -0400

    added delegates labs

---
+++ Labs/DelLab01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DelLab01
+{
+    //1. declare a delegate type
+    delegate void MyDel();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Del20200329a");
+            //2. instantiate a delegate object
+            //either of the two lines below, a or b
+            //var methodDelegate = new MyDel(SomeMethod); //a
+            MyDel methodDelegate = SomeMethod; //b
+            //3. call the delegate
+            methodDelegate();
+        }
+
+        static void SomeMethod()
+        {
+            Console.WriteLine("This is the body of the method named 'someMethod()'");
+        }
+    }
+}
